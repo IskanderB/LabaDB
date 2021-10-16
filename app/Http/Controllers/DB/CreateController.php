efb380b886bf6b5ef505a36919c9e3ba4e75e9e3
Alexandr Khurtin <axurtin.rep@gmail.com>
2021-10-16 11:46:20 +0300

    Start Backend

---
+++ app/Http/Controllers/DB/CreateController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\DB;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CreateController extends Controller
+{
+    public function create(Request $request) {
+
+    }
+}
