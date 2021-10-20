fa706b3386a055af6a7523db21c21d4e69c77375
Alexandr Khurtin <axurtin.rep@gmail.com>
2021-10-20 19:57:11 +0300

    Added middleware HttpsProtocol, switch down web routes

--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/testbl', 'App\Http\Controllers\TestBL\PageController@index')->name('testbl');
-
-Route::name('testbl.')->group(function () {
-    Route::post('/testbl/makedir', 'App\Http\Controllers\TestBL\BLController@makedir')->name('makedir');
-    Route::post('/testbl/makefiles', 'App\Http\Controllers\TestBL\BLController@makefiles')->name('makefiles');
-    Route::post('/testbl/rmdir', 'App\Http\Controllers\TestBL\BLController@rmdir')->name('rmdir');
-    Route::post('/testbl/cleardir', 'App\Http\Controllers\TestBL\BLController@cleardir')->name('cleardir');
-    Route::get('/testbl/insert', 'App\Http\Controllers\TestBL\BLController@insert')->name('insert');
-    Route::get('/testbl/select', 'App\Http\Controllers\TestBL\BLController@select')->name('select');
-    Route::get('/testbl/delete', 'App\Http\Controllers\TestBL\BLController@delete')->name('delete');
-    Route::get('/testbl/edit', 'App\Http\Controllers\TestBL\BLController@edit')->name('edit');
-    Route::get('/testbl/getbackup', 'App\Http\Controllers\TestBL\BLController@getBackup')->name('getbackup');
-    Route::post('/testbl/restore', 'App\Http\Controllers\TestBL\BLController@restore')->name('restore');
-    Route::post('/testbl/uplbackup', 'App\Http\Controllers\TestBL\BLController@uplbackup')->name('uplbackup');
-    Route::post('/testbl/createbackup', 'App\Http\Controllers\TestBL\BLController@createbackup')->name('createbackup');
-    Route::post('/testbl/import', 'App\Http\Controllers\TestBL\BLController@import')->name('import');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+//Route::get('/testbl', 'App\Http\Controllers\TestBL\PageController@index')->name('testbl');
+//
+//Route::name('testbl.')->group(function () {
+//    Route::post('/testbl/makedir', 'App\Http\Controllers\TestBL\BLController@makedir')->name('makedir');
+//    Route::post('/testbl/makefiles', 'App\Http\Controllers\TestBL\BLController@makefiles')->name('makefiles');
+//    Route::post('/testbl/rmdir', 'App\Http\Controllers\TestBL\BLController@rmdir')->name('rmdir');
+//    Route::post('/testbl/cleardir', 'App\Http\Controllers\TestBL\BLController@cleardir')->name('cleardir');
+//    Route::get('/testbl/insert', 'App\Http\Controllers\TestBL\BLController@insert')->name('insert');
+//    Route::get('/testbl/select', 'App\Http\Controllers\TestBL\BLController@select')->name('select');
+//    Route::get('/testbl/delete', 'App\Http\Controllers\TestBL\BLController@delete')->name('delete');
+//    Route::get('/testbl/edit', 'App\Http\Controllers\TestBL\BLController@edit')->name('edit');
+//    Route::get('/testbl/getbackup', 'App\Http\Controllers\TestBL\BLController@getBackup')->name('getbackup');
+//    Route::post('/testbl/restore', 'App\Http\Controllers\TestBL\BLController@restore')->name('restore');
+//    Route::post('/testbl/uplbackup', 'App\Http\Controllers\TestBL\BLController@uplbackup')->name('uplbackup');
+//    Route::post('/testbl/createbackup', 'App\Http\Controllers\TestBL\BLController@createbackup')->name('createbackup');
+//    Route::post('/testbl/import', 'App\Http\Controllers\TestBL\BLController@import')->name('import');
+//});
